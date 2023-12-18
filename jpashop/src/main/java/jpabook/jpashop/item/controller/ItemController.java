25772e25c98fd2753417d56e5217458ad5352a8a
Drum-J <tmdgh717@naver.com>
2023-12-18 15:58:44 +0900

    섹션 7. 웹 계층 개발 / 상품 수정까지

---
+++ jpashop/src/main/java/jpabook/jpashop/item/controller/ItemController.java
@@ -0,0 +1,84 @@
+package jpabook.jpashop.item.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.item.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService service;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/new")
+    public String create(BookForm form) {
+
+        // 여기는 Setter 를 사용했으나 생성자 메서드를 가지고 만드는게 더 깔끔하다!
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        service.save(book);
+
+        return "redirect:/items";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<Item> items = service.findAll();
+        model.addAttribute("items", items);
+
+        return "items/itemList";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) service.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String updateItems(@ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        service.save(book);
+
+        return "redirect:/items";
+    }
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/member/controller/MemberController.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.member.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService service;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        service.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        // Meber 엔티티를 그대로 뿌리기 보다는 DTO로 변환해서 필요한 부분만 화면에 뿌려주자!
+        List<Member> members = service.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
+
+}
